028fda397ac08e895c24c41d8740db3af1151756
rojhat <rojhatbirel43821@gmail.com>
2024-04-04 23:06:41 +0300

    App.js ve Currentweather.js dosyalarında yapılan değişiklikler

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./output.css";
 import Logo from "./components/Logo";
 import Greetings from "./components/Greetings";
 import Searchbar from "./components/Searchbar";
 import Currentweather from "./components/Currentweather";
 import Weatherdetail from "./components/Weatherdetail";
-import { useState } from "react";
 import { weatherAPI } from "./api";
 import axios from "axios";
 
 function App() {
-  const [currentweather, setCurrentweather] = useState(null);
+  const [currentWeatherData, setCurrentWeatherData] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+
   const handleOnSearch = (searchData) => {
-    console.log(searchData);
+    setSelectedCity(searchData.label);
     const [lat, lon] = searchData.value.split(" ");
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherAPI}&units=metric`
       )
       .then((response) => {
-        console.log(response.data);
+        setCurrentWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data: ", error);
+        setCurrentWeatherData(null); // Hata durumunda veriyi temizle
       });
   };
 
   return (
     <div className="font-nunito">
-      <div className=" bg-background h-full">
+      <div className="bg-background h-screen">
         <Logo />
-        <Greetings />
+        {(!currentWeatherData || !selectedCity) && ( // Veri veya seçili şehir yoksa Greetings bileşenini göster
+          <Greetings />
+        )}
         <Searchbar onSearchChange={handleOnSearch} />
-        <Currentweather />
-        <Weatherdetail />
+        {currentWeatherData &&
+          selectedCity && ( // Veri ve seçili şehir varsa Currentweather bileşenini göster
+            <Currentweather data={currentWeatherData} city={selectedCity} />
+          )}
       </div>
     </div>
   );
--- src/components/Currentweather.js
+++ src/components/Currentweather.js
@@ -12,23 +12,67 @@ import stormday from "../Assets/Weather/stormday.svg";
 import stormnight from "../Assets/Weather/stormnight.svg";
 import snowy from "../Assets/Weather/snow.svg";
 
-function Currentweather() {
+function Currentweather({ data, city }) {
+  let weatherIcon;
+  let weatherDescription;
+  let temperature;
+  let maxTemperature;
+  let minTemperature;
+  let countryCode;
+  let cityName;
+
+  console.log(data);
+  // Veri varsa, verileri değişkenlere atayalım
+  if (data) {
+    weatherDescription = data.weather[0].description;
+    temperature = Math.round(data.main.temp);
+    maxTemperature = Math.round(data.main.temp_max);
+    minTemperature = Math.round(data.main.temp_min);
+
+    // Hava durumu ikonunu belirleme
+    switch (data.weather[0].main) {
+      case "Clear":
+        weatherIcon = clearday;
+        break;
+      case "Clouds":
+        weatherIcon = cloudday;
+        break;
+      case "Rain":
+        weatherIcon = rainday;
+        break;
+      case "Thunderstorm":
+        weatherIcon = stormday;
+        break;
+      case "Snow":
+        weatherIcon = snowy;
+        break;
+      default:
+        weatherIcon = clearday;
+    }
+  }
+
   return (
     <div className="text-white mt-10">
       <div className="current-weather-card h-80 w-96 bg-custom-blue-transparent m-auto rounded-md flex justify-center items-center">
         <div className="weather-detail rounded-lg h-72 w-90 bg-clearnight flex flex-col justify-between">
           <div className="text-left pl-4 pt-4">
-            <p className="text-4xl font-bold mb-2">Izmir, TR</p>
-            <p className=" text-xl  font-extralight">Clear</p>
+            <p className="text-4xl font-bold mb-2">{city}</p>
+            <p className=" text-xl  font-extralight">{weatherDescription}</p>
           </div>
           <div className="flex flex-row justify-end items-center gap-1">
             <div className=" mr-20">
-              <p className=" text-4xl font-bold">22°</p>
-              <p className=" text-lg font-thin">22°/10°</p>
-              <p>few clouds</p>
+              <p className=" text-4xl font-bold">{temperature}°</p>
+              <p className=" text-lg font-thin">
+                {maxTemperature}°/{minTemperature}°
+              </p>
+              <p>{weatherDescription}</p>
             </div>
             <div className="">
-              <img src={clearday} alt="" className="w-150 " />
+              <img
+                src={weatherIcon}
+                alt={weatherDescription}
+                className="w-150 "
+              />
             </div>
           </div>
         </div>
